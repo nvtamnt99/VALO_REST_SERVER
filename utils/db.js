c62a51f5f314635407479cc56d5add021f0ba88d
nvtamnt99 <nvtamnt99>
2021-10-03 10:37:27 +0700

    refactor code

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,3 @@
-// const knex = require('knex')({
-//     client: 'mysql2',
-//     connection: {
-//       host : 'localhost',
-//       port : 3306,
-//       user : 'root',
-//       password : '311299',
-//       database : 'valo_chat_clone'
-//     }
-//   });
-
-//   module.exports = knex;
-
 module.exports = {
   HOST: "localhost",
   USER: "root",
